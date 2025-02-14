0d54e245e42d723f291133ad8750fee168c9611c
MosesX <152671307+Momossini@users.noreply.github.com>
2025-02-14 23:39:57 +0100

    Add initial learning activities and welcome program for CSE 110

---
+++ csepc110_introduction_to_programming/w01_getting_started_and_Input_output/w01_activities/favorite_color.py
@@ -0,0 +1,8 @@
+"""
+Author: Moïse Marc-Aurel MASSAMBA-DILUEKI
+
+Purpose: Asking the user to input their favorite color and then displaying it to the screen.
+"""
+
+favorite_color = input("Please type your favorite color: ")
+print(f"Your favorite color is {favorite_color}")
